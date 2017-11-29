9f443914cc0f3d5a65b2c0d66802e621366bc373
Parker Catalano <catalano.work@gmail.com>
2017-11-29 17:36:02 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,12 @@
 class Appointment
-  attr_accessor :date, :doctor
+  attr_accessor :date, :doctor, :patient 
 
   def initialize(date, doctor)
     @date = date
     @doctor = doctor
   end
+
+  def patient
+  
   
 end
